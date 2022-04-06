b6a54e1dc0b5bf6514abeac6d471d5d356bda512
Hung <rememberminato@gmail.com>
2022-04-06 23:39:02 +0700

    remove file

--- bc16-baitapkinh-duongtienhung/src/App.js
+++ bc16-baitapkinh-duongtienhung/src/App.js
@@ -1,25 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import CardJSX from "./Component/DemoComponent/CardJSX";
-import CardFunc from "./Component/DemoComponent/CardFunc";
-import BaiTapLayout from "./Component/BaiTapLayout/BaiTapLayout";
-import Databinding from "./Databinding/Databinding";
-import HandleEvent from "./HandleEvent/HandleEvent";
-import StateDemo from "./StateDemo/StateDemo";
-import RenderWithMap from "./RenderWithMap/RenderWithMap";
 import BaiTapThuKinh from "./BaiTapThuKinh/BaiTapThuKinh";
 //Component App sẽ là nơi chứa toàn bộ giao diện của ứng dụng
 function App() {
   return (
     <div className="App">
-      {/* <CardFunc />
-        <CardJSX /> */}
-      {/* <BaiTapLayout /> */}
-      {/* <Databinding /> */}
-      {/* <HandleEvent /> */}
-      {/* <StateDemo /> */}
-      {/* <RenderWithMap/> */}
-      <BaiTapThuKinh/>
+      <BaiTapThuKinh />
     </div>
   );
 }
